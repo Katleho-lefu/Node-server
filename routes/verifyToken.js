9b010166b6ac1bf290fea7b966f4687ea7ffb5e4
katdarad <katdarad18@gmail.com>
2023-06-01 16:31:16 +0200

    Have to finish all queries on postman

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -2,11 +2,9 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
-  console.log("auth header", authHeader);
   if (authHeader) {
     const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_KEY, (err, user) => {
-        console.log(token)
       if (err) res.status(403).json("Token is not valid!");
       req.user = user;
       next();
